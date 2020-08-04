ef46dc079fd1639586ec088dadca02e6897803ea
Wheaties466 <Mwheat42289@gmail.com>
2020-08-04 16:37:02 -0400

    Update miner_latest.sh
    syntax adjustments from tteague
    
    Additional comments if you install the wrong miner architecture like i did 😆

--- miner_latest.sh
+++ miner_latest.sh
@@ -5,13 +5,19 @@
 
 ARCH=arm
 
+#if you're running the miner on amd64 based architecture uncomment below
+#if you accidently install the wrong miner software, the miner will immediately crash and you will need to remove the bad image
+#docker stop miner && docker rm miner && docker image ls
+#remove the bad image with "docker image rm [IMAGE ID]
+#ARCH=amd
+
 miner_latest=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=100&page=1&onlyActiveTags=true' | jq -c --arg ARCH "$ARCH" '[ .tags[] | select( .name | contains($ARCH)) ][0].name' | cut -d'"' -f2)
 
 
-if ` echo $miner_latest | grep -q $ARCH`;
+if `echo $miner_latest | grep -q $ARCH`;
 then echo "Latest miner version" $miner_latest;
 elif miner_latest=$(curl -s 'https://quay.io/api/v1/repository/team-helium/miner/tag/?limit=100&page=1&onlyActiveTags=true' | jq -r .tags[1].name)
-echo "Latest miner version" $miner_latest;
+then echo "Latest miner version" $miner_latest;
 fi
 
 running_image=$(docker images quay.io/team-helium/miner:$miner_latest -q)
