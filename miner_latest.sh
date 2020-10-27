db5df484d960879087e93dd4490c3c9c9e1361a6
Wheaties466 <Mwheat42289@gmail.com>
2020-10-27 19:50:24 -0400

    Update miner_latest.sh
    adding in an echo statement for additional logging.

--- miner_latest.sh
+++ miner_latest.sh
@@ -60,7 +60,7 @@ current_height=$(curl -s https://api.helium.io/v1/blocks/height | jq .data.heigh
 miner_height=$(docker exec "$MINER" miner info height | awk '{print $2}')
 height_diff=$(expr "$current_height" - "$miner_height")
 
-if [[ $height_diff -gt 50 ]]; then docker stop "$MINER" && docker start "$MINER" ; fi
+if [[ $height_diff -gt 50 ]]; then docker stop "$MINER" && docker start "$MINER" && echo "stopping and starting the miner because it may be stuck syncing the blockchain" ; fi
 
 #If the miner is more than 500 blocks behind, stop the image, remove the container, remove the image. It will be redownloaded later in the script.
 if [[ $height_diff -gt 500 ]]; then docker stop "$MINER" && docker rm "$MINER" && docker image rm "$miner_latest" ; fi
