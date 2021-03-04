382d72df7e106da3370f7def54d822e41802b5fe
Ubuntu <ubuntu@ns532058.ip-198-245-61.net>
2021-03-04 21:53:10 +0000

    fixing issues with wallet staking

--- validator/wallet_stake.sh
+++ validator/wallet_stake.sh
@@ -5,28 +5,30 @@ CONFIG_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
 #you will need to copy config.txt.tmp to config.txt to configure additional variables, this will make it sure the git update works properly
 
 source $CONFIG_DIR/config.txt
- 
+
+#for debuigging
+#set -x
 
 command -v locate > /dev/null || sudo apt-get install mlocate -y && sudo updatedb
 
-if [ $WALLET_BIN -z ]; then
+if [ -z "$WALLET_BIN" ]; then
 	WALLET_BIN=$(locate bin/helium-wallet | head -n 1)
 fi
 
-if [ $WALLET_KEY -z ]; then
+if [ -z "$WALLET_KEY" ]; then
         WALLET_KEY=$(locate wallet.key | head -n 1)
 fi
-if [ $WALLET_KEY -z ]; then
+if [ -z "$WALLET_KEY" ]; then
 	$WALLET_BIN create basic --network $NETWORK
 	sudo updatedb
         WALLET_KEY=$(locate wallet.key | head -n 1)
 fi
 
-WALLET_BALANCE=$($WALLET_BIN -f $WALLET_KEY info | grep -i balance | grep -v "DC\|Securities" | awk '{print $4}')
+WALLET_BALANCE=`$WALLET_BIN -f $WALLET_KEY info | grep -i balance | grep -v "DC\|Securities" | awk '{print $4}'`
 
-WALLET_ADDR=$(/home/helium/helium-wallet-rs/bin/helium-wallet -f $WALLET_KEY info | grep "Address" | awk '{print $4}')
+WALLET_ADDR=$($WALLET_BIN -f $WALLET_KEY info | grep "Address" | awk '{print $4}')
 
-if [[ $WALLET_BALLANCE -lt 10000 ]]; then
+if [[ $WALLET_BALANCE -lt 10000 ]]; then
 	echo " Wallet balance is less than 10k, go to https://faucet.helium.wtf/ to re up. submit the following wallet address" $WALLET_ADDR
 	exit 0
 fi
